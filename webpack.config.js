e71ea1a8cd2e100d3856f5a37162fbfa0daad892
Andrew Mager <mager@postmates.com>
2016-06-12 17:22:26 -0700

    Set up hot reloading and babel

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,26 @@
 module.exports = {
     entry: [
-        './src/index.js'
+        './src/index.js',
+        'webpack-dev-server/client?http://localhost:8080',
+        'webpack/hot/only-dev-server',
     ],
+    module: {
+        loaders: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'react-hot!babel'
+        }]
+    },
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
     output: {
         path: __dirname + '/dist',
         publicPath: '/',
         filename: 'bundle.js'
     },
     devServer: {
-        contentBase: './dist'
+        contentBase: './dist',
+        hot: true
     }
 };
