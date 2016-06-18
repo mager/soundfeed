17adb0996ec3bb17c46f6f1a2eda1da975f88c00
Andrew Mager <mager@postmates.com>
2016-06-18 10:51:57 -0700

    Adding tests

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-console.log('Soundfeed is alive.');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Stream from './components/Stream';
+
+const tracks = [
+    {
+        title: 'My first track'
+    },
+    {
+        title: 'My second track'
+    }
+];
+
+ReactDOM.render(
+    <Stream tracks={tracks} />,
+    document.getElementById('app')
+);
