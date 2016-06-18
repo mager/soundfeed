580bbdff51bcebc8bfcad03ab15918b82ce353b8
Andrew Mager <mager@postmates.com>
2016-06-18 15:50:13 -0700

    Login with Soundcloud

--- src/components/Stream/presenter.js
+++ src/components/Stream/presenter.js
@@ -1,15 +1,18 @@
 import React from 'react';
 
-function Stream({ tracks = [] }) {
+function Stream({ tracks = [], onAuth }) {
     return (
         <div>
-            {
-                tracks.map((track, key) => {
+            <div>
+                <button onClick={onAuth} type="button">Login</button>
+            </div>
+            <div>
+                {tracks.map((track, key) => {
                     return (
                         <div className="track" key={key}>{track.title}</div>
                     );
-                })
-            }
+                })}
+            </div>
         </div>
     );
 }
